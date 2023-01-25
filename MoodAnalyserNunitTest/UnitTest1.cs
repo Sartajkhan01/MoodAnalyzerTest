0e1a60ea34399dc9ad319b9727a3dff9feb27fd1
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-25 22:59:56 +0530

    [Sartaj] : Custom exception unit test program

--- MoodAnalyserNunitTest/UnitTest1.cs
+++ MoodAnalyserNunitTest/UnitTest1.cs
@@ -68,6 +68,42 @@ namespace MoodAnalyserNunitTest
             string message = moodAnalyser.AnalyseMood();
             Assert.AreEqual("HAPPY", message);
         }
+        //UC-3
+        /// <summary>
+        /// TC3.1 Given NULL Mood Should Throw MoodAnalysisException To pass this Test Case
+        /// in try catch block throw MoodAnalysisException
+        /// </summary>
+        [TestMethod]
+        public void GivenMessage_WhenNull_USingCustomException_ShouldReturnNullMood()
+        {
+            moodAnalyser = new MoodAnalyser();
+            try
+            {
+                string message = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserCustomException exception)
+            {
+                Assert.AreEqual(MoodAnalyserCustomException.ExceptionType.NULL_MOOD, exception.exceptionType);
+            }
 
+        }
+        /// <summary>
+        /// TC3.2 Given Empty Mood Should Throw MoodAnalysisException indicating Empty Mood 
+        /// Handle Empty Mood Scenario throw MoodAnalysisException and inform user of the EmptyMood 
+        /// HINT: Use Enum to EMPTY or NULL
+        /// </summary>
+        [TestMethod]
+        public void GivenMessage_WhenEmpty_USingCustomException_ShouldReturnNullMood()
+        {
+            moodAnalyser = new MoodAnalyser("");
+            try
+            {
+                string message = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserCustomException exception)
+            {
+                Assert.AreEqual(MoodAnalyserCustomException.ExceptionType.EMPTY_MOOD, exception.exceptionType);
+            }
+        }
     }
 }
